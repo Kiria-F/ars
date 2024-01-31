0c34f7c68fbb773e65c0b006188f0457237f5787
Sedo-KFM <fedor122448@gmail.com>
2024-01-31 12:47:09 +0400

    Add DI and ClientTestService
    (cherry picked from commit 8ad8f60c0732ce8dc9bd54936dd3b825d70753c2)

---
+++ Controllers/ClientTestController.cs
@@ -0,0 +1,19 @@
+﻿using ARS.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ARS.Controllers; 
+
+[Route("test")]
+public class ClientTestController : Controller {
+    
+    private readonly IClientTestService _clientTestService;
+    
+    public ClientTestController(IClientTestService clientTestService) {
+        _clientTestService = clientTestService;
+    }
+    
+    // GET
+    public uint Index() {
+        return _clientTestService.Trigger();
+    }
+}
