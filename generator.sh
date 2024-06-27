3e26dda1caabfec51a4d797c863d96ce90213311
Fedor <fedor122448@gmail.com>
2024-06-28 00:31:09 +0400

    Add shell directive to generator.sh

--- generator.sh
+++ generator.sh
@@ -1,4 +1,6 @@
-﻿for file_i in $( seq 0 $(( $(jq -r ".[\"$1\"] | length" secrets.json ) - 1 )) ); do
+﻿#!/usr/bin/bash
+
+for file_i in $( seq 0 $(( $(jq -r ".[\"$1\"] | length" secrets.json ) - 1 )) ); do
     filename=$( jq -r ".[\"$1\"][$file_i][\"filename\"]" secrets.json )
     target=$( jq -r ".[\"$1\"][$file_i][\"target\"]" secrets.json )
     keys_count=$( jq -r ".[\"$1\"][$file_i][\"keys\"] | length" secrets.json )
