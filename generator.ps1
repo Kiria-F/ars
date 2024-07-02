9ac2c82868798608f94a1620423e92562303d417
Fedor <fedor122448@gmail.com>
2024-07-03 01:50:34 +0400

    Update secrets structure

--- generator.ps1
+++ generator.ps1
@@ -1,6 +1,18 @@
-﻿$secrets = Get-Content -Path "secrets.json" | ConvertFrom-Json
-foreach ($config in $secrets.local) {
+﻿param([string]$setup)
+Write-Output "Generating secrets for $setup..."
+$secrets = Get-Content -Path "secrets.json" | ConvertFrom-Json
+foreach ($config in $secrets.$setup) {
     $filename = $config.filename
+    $target = $config.target
+    $keys = $config.keys | ConvertTo-Json | ConvertFrom-Json -AsHashtable
+    $content = Get-Content -Path $filename
+    foreach ($key in $keys.Keys) {
+        $content = $content -replace "\%$key\%", $keys[$key]
+    }
+    $content | Set-Content -Path $target
+}
+foreach ($config in $secrets.shared) {
+    $filename = $config.target
     $target = $config.target
     $keys = $config.keys | ConvertTo-Json | ConvertFrom-Json -AsHashtable
     $content = Get-Content -Path $filename
--- generator.sh
+++ generator.sh
@@ -5,9 +5,20 @@ for file_i in $( seq 0 $(( $(jq -r ".[\"$1\"] | length" secrets.json ) - 1 )) );
     target=$( jq -r ".[\"$1\"][$file_i][\"target\"]" secrets.json )
     keys_count=$( jq -r ".[\"$1\"][$file_i][\"keys\"] | length" secrets.json )
     cp "$filename" "$target"
-    for key_i in $( seq 0 $(( $keys_count - 1 )) ); do
+    for key_i in $( seq 0 $(( keys_count - 1 )) ); do
         key=$( jq -r ".[\"$1\"][$file_i][\"keys\"] | keys | .[$key_i]" secrets.json )
         value=$( jq -r ".[\"$1\"][$file_i][\"keys\"][\"$key\"]" secrets.json )
+        value=${value//\//\\\/}
+        sed -i "s/%$key%/$value/" "$target"
+    done
+done
+for file_i in $( seq 0 $(( $(jq -r ".[\"shared\"] | length" secrets.json ) - 1 )) ); do
+    target=$( jq -r ".[\"shared\"][$file_i][\"target\"]" secrets.json )
+    keys_count=$( jq -r ".[\"shared\"][$file_i][\"keys\"] | length" secrets.json )
+    touch "$target"
+    for key_i in $( seq 0 $(( keys_count - 1 )) ); do
+        key=$( jq -r ".[\"shared\"][$file_i][\"keys\"] | keys | .[$key_i]" secrets.json )
+        value=$( jq -r ".[\"shared\"][$file_i][\"keys\"][\"$key\"]" secrets.json )
         value=${value//\//\\\/}
         sed -i "s/%$key%/$value/" "$target"
     done
